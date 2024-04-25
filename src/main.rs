5bfbafce82b80844278e0be9bea7bc8223d547bf
Turbo Fredriksson <turbo@bayour.com>
2024-04-25 09:48:05 +0100

    Move the WS2812 part out to a separate file.
    This way we can use it as a module.
    + Setup and send a signal to the tasks to sleep longer every time
      the main loop restarts.

--- src/main.rs
+++ src/main.rs
@@ -5,120 +5,32 @@
 #![no_std]
 #![no_main]
 
-
 use defmt::*;
+
 use embassy_executor::Spawner;
 use embassy_rp::gpio::{AnyPin, Level, Output, Pin};
-use embassy_rp::dma::{AnyChannel, Channel};
+use embassy_rp::bind_interrupts;
 use embassy_rp::peripherals::PIO0;
-use embassy_rp::pio::{
-    Common, Config, FifoJoin, Instance, InterruptHandler, Pio, PioPin, ShiftConfig,
-    ShiftDirection, StateMachine
-};
-use embassy_rp::{bind_interrupts, clocks, into_ref, Peripheral, PeripheralRef};
+use embassy_rp::pio::{InterruptHandler, Pio};
 use embassy_time::{Duration, Ticker, Timer};
+use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
+use embassy_sync::channel::{Channel as SyncChannel, Receiver};
 
-use fixed::types::U24F8;
-use fixed_macro::fixed;
+pub mod ws2812;
+use crate::ws2812::Ws2812;
+
 use smart_leds::RGB8;
-
 use {defmt_rtt as _, panic_probe as _};
 
-// ================================================================================
+enum SleepLonger { Yes }
+static CHANNEL1: SyncChannel<ThreadModeRawMutex, SleepLonger, 64> = SyncChannel::new();
+static CHANNEL2: SyncChannel<ThreadModeRawMutex, SleepLonger, 64> = SyncChannel::new();
+static CHANNEL3: SyncChannel<ThreadModeRawMutex, SleepLonger, 64> = SyncChannel::new();
+static CHANNEL4: SyncChannel<ThreadModeRawMutex, SleepLonger, 64> = SyncChannel::new();
 
 bind_interrupts!(struct Irqs {
     PIO0_IRQ_0 => InterruptHandler<PIO0>;
 });
-
-pub struct Ws2812<'d, P: Instance, const S: usize, const N: usize> {
-    dma: PeripheralRef<'d, AnyChannel>,
-    sm: StateMachine<'d, P, S>,
-}
-
-impl<'d, P: Instance, const S: usize, const N: usize> Ws2812<'d, P, S, N> {
-    pub fn new(
-        pio: &mut Common<'d, P>,
-        mut sm: StateMachine<'d, P, S>,
-        dma: impl Peripheral<P = impl Channel> + 'd,
-        pin: impl PioPin,
-    ) -> Self {
-        into_ref!(dma);
-
-        // Setup sm0
-
-        // prepare the PIO program
-        let side_set = pio::SideSet::new(false, 1, false);
-        let mut a: pio::Assembler<32> = pio::Assembler::new_with_side_set(side_set);
-
-        const T1: u8 = 2; // start bit
-        const T2: u8 = 5; // data bit
-        const T3: u8 = 3; // stop bit
-        const CYCLES_PER_BIT: u32 = (T1 + T2 + T3) as u32;
-
-        let mut wrap_target = a.label();
-        let mut wrap_source = a.label();
-        let mut do_zero = a.label();
-        a.set_with_side_set(pio::SetDestination::PINDIRS, 1, 0);
-        a.bind(&mut wrap_target);
-        // Do stop bit
-        a.out_with_delay_and_side_set(pio::OutDestination::X, 1, T3 - 1, 0);
-        // Do start bit
-        a.jmp_with_delay_and_side_set(pio::JmpCondition::XIsZero, &mut do_zero, T1 - 1, 1);
-        // Do data bit = 1
-        a.jmp_with_delay_and_side_set(pio::JmpCondition::Always, &mut wrap_target, T2 - 1, 1);
-        a.bind(&mut do_zero);
-        // Do data bit = 0
-        a.nop_with_delay_and_side_set(T2 - 1, 0);
-        a.bind(&mut wrap_source);
-
-        let prg = a.assemble_with_wrap(wrap_source, wrap_target);
-        let mut cfg = Config::default();
-
-        // Pin config
-        let out_pin = pio.make_pio_pin(pin);
-        cfg.set_out_pins(&[&out_pin]);
-        cfg.set_set_pins(&[&out_pin]);
-
-        cfg.use_program(&pio.load_program(&prg), &[&out_pin]);
-
-        // Clock config, measured in kHz to avoid overflows
-        // TODO CLOCK_FREQ should come from embassy_rp
-        let clock_freq = U24F8::from_num(clocks::clk_sys_freq() / 1000);
-        let ws2812_freq = fixed!(800: U24F8);
-        let bit_freq = ws2812_freq * CYCLES_PER_BIT;
-        cfg.clock_divider = clock_freq / bit_freq;
-
-        // FIFO config
-        cfg.fifo_join = FifoJoin::TxOnly;
-        cfg.shift_out = ShiftConfig {
-            auto_fill: true,
-            threshold: 24,
-            direction: ShiftDirection::Left,
-        };
-
-        sm.set_config(&cfg);
-        sm.set_enable(true);
-
-        Self {
-            dma: dma.map_into(),
-            sm,
-        }
-    }
-
-    pub async fn write(&mut self, colors: &[RGB8; N]) {
-        // Precompute the word bytes from the colors
-        let mut words = [0u32; N];
-        for i in 0..N {
-            let word = (u32::from(colors[i].g) << 24) | (u32::from(colors[i].r) << 16) | (u32::from(colors[i].b) << 8);
-            words[i] = word;
-        }
-
-        // DMA transfer
-        self.sm.tx().dma_push(self.dma.reborrow(), &words).await;
-
-        Timer::after_micros(55).await;
-    }
-}
 
 // ================================================================================
 
@@ -140,7 +52,7 @@ fn wheel(mut wheel_pos: u8) -> RGB8 {
 // A pool size of 4 means you can spawn four instances of this task.
 // One for each status LED.
 #[embassy_executor::task(pool_size = 4)]
-async fn led_blink(pin: AnyPin) {
+async fn led_blink(control: Receiver<'static, ThreadModeRawMutex, SleepLonger, 64>, pin: AnyPin) {
     let mut led = Output::new(pin, Level::Low);
 
     loop {
@@ -148,7 +60,17 @@ async fn led_blink(pin: AnyPin) {
 	Timer::after_millis(150).await;
 
 	led.set_low();
-	Timer::after_millis(150).await;
+
+	match control.try_receive() {
+	    core::prelude::v1::Ok(SleepLonger::Yes) => {
+		info!("SleepLonger = Yes received, sleeping 3s");
+		Timer::after_secs(3).await;
+	    },
+	    _ => {
+		info!("Nothing received, sleeping 150ms");
+		Timer::after_millis(150).await;
+	    }
+	}
     }
 }
 
@@ -162,13 +84,13 @@ async fn main(spawner: Spawner) {
 
     // =====
 
-    spawner.spawn(led_blink(p.PIN_6.degrade())).unwrap();
+    spawner.spawn(led_blink(CHANNEL1.receiver(), p.PIN_6.degrade())).unwrap();
     Timer::after_secs(1).await;
-    spawner.spawn(led_blink(p.PIN_7.degrade())).unwrap();
+    spawner.spawn(led_blink(CHANNEL2.receiver(), p.PIN_7.degrade())).unwrap();
     Timer::after_secs(1).await;
-    spawner.spawn(led_blink(p.PIN_8.degrade())).unwrap();
+    spawner.spawn(led_blink(CHANNEL3.receiver(), p.PIN_8.degrade())).unwrap();
     Timer::after_secs(1).await;
-    spawner.spawn(led_blink(p.PIN_9.degrade())).unwrap();
+    spawner.spawn(led_blink(CHANNEL4.receiver(), p.PIN_9.degrade())).unwrap();
     Timer::after_secs(1).await;
 
     // This is the number of leds in the string. Helpfully, the sparkfun thing plus and adafruit
@@ -182,6 +104,15 @@ async fn main(spawner: Spawner) {
     loop {
 	info!("NeoPixel off");
 	ws2812.write(&[(0,0,0).into()]).await;
+
+	// Tell the LEDs to sleep longer.
+	CHANNEL1.send(SleepLonger::Yes).await;
+	Timer::after_secs(1).await;
+	CHANNEL2.send(SleepLonger::Yes).await;
+	Timer::after_secs(1).await;
+	CHANNEL3.send(SleepLonger::Yes).await;
+	Timer::after_secs(1).await;
+	CHANNEL4.send(SleepLonger::Yes).await;
 
 	// =====
 	
